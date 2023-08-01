9265a40f924f1045d1d5062a05bce422efd786f5
Ben McHone <ben@mchone.dev>
2023-07-31 21:49:02 -0500

    Evaluate prefix expressions

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/benmch/chip-8-go/internal/evaluator"
 	"github.com/benmch/chip-8-go/internal/lexer"
 	"github.com/benmch/chip-8-go/internal/parser"
 )
@@ -32,8 +33,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
