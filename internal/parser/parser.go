9a6fbdfd725591981178236470a3b9f688acaede
Ben McHone <ben@mchone.dev>
2023-07-31 20:53:01 -0500

    Removed TODOs

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -334,8 +334,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: Skipping the tokens until we encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -347,8 +350,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: Skipping the tokens until we encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+	
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
